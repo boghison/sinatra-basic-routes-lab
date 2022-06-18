44627deb3f0db89c8c47c4808912da374eff279d
Bogdan Cuza <bogdancuza@protonmail.com>
2022-06-18 16:48:48 +0300

    commit tree 66e2d93f6618e504b33945a2ab0cbcd43a9f7d51

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get "/name" do
+        "My name is Bogdan"
+    end
+
+    get "/hometown" do
+        "My hometown is Chișinău"
+    end
+
+    get "/favorite-song" do
+        "My favorite song is Gotta Go by Chung Ha"
+    end
 end
